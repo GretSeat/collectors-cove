38df4ead4c33799f759a53440170f4d0add5c11d
Garrett Stewart <garrettbstewart@gmail.com>
2023-10-16 11:47:51 -0400

    template literals?

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
   // Opening for Try Catch
   try {
     // await to process the env file database string
-    const conn = await mongoose.connect(process.env.DB_STRING);
+    const conn = await mongoose.connect(`${process.env.DB_STRING}`);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`); // Console log to tell us MongoDB is connected
     // Opening for Catch
