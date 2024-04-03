55dfd76022c8f1dbdff60cd492b2790def8b9728
Garrett Stewart <gretseatdev@gmail.com>
2024-04-03 13:51:34 -0400

    WOrking on Disbound on Different Github Account, Converting to React - Fixed Form Value

--- collectors-cove2.0/src/components/Form.jsx
+++ collectors-cove2.0/src/components/Form.jsx
@@ -24,18 +24,21 @@ export default function Form() {
         placeholder="First Name"
         onChange={handleChange}
         name="firstName"
+        value="formData.firstName"
       />
       <input
         type="text"
         placeholder="Last Name"
         onChange={handleChange}
         name="lastName"
+        value="formData.lastName"
       />
       <input
         type="email"
         placeholder="Email"
         onChange={handleChange}
         name="email"
+        value="formData.email"
       />
     </form>
   );
